75aad04f3f9f81d3b24720f6a6f46a8716578824
mkotsollaris <mkotsollari@gmail.com>
2019-12-19 17:27:48 -0800

    upgraded TruliooClient

--- src/TruliooClient.js
+++ src/TruliooClient.js
@@ -4,5 +4,31 @@ export default class TruliooClient {
         console.log('mpika!', publicKey, accessTokenGeneratorURL)
         this.publicKey = publicKey;
         this.accessTokenGeneratorURL = accessTokenGeneratorURL;
+        this.loadEmbedID();
+    }
+
+    loadEmbedID() {
+        const embedIDBackendURL = "http://localhost:8855/embedid/";
+        const element = document.createElement('iframe');
+        element.setAttribute('id', 'embedid-module');
+        element.setAttribute('src', `${embedIDBackendURL}${this.publicKey}`);
+        document.getElementById('trulioo-embedid').appendChild(element);
+
+        window.addEventListener('message', async (e) => {
+            const originURL = 'http://localhost:8855'; //embedID BE URL
+            if (e.origin === originURL) {
+                const response = await fetch(`${this.accessTokenGeneratorURL}`);
+                const accessToken = JSON.parse(response.data.AccessToken);
+                //console.log('Sending Access Access Token fron client to EmbedID-BE:', accessToken);
+                //e.source.postMessage(`Ihackedyou;D`, '*');
+                //TODO change
+                e.source.postMessage(`${accessToken}`, '*');
+            }
+        });
+
+        const embedIDModule = document.getElementById("embedid-module");
+        embedIDModule.style.width = "100%";
+        embedIDModule.style.height = "100%";
+        embedIDModule.border = "none";
     }
 }
--- src/index.js
+++ src/index.js
@@ -5,32 +5,33 @@ import TruliooClient from './TruliooClient.js';
 //     "PQoy64YSbLQR25aJ",
 //     "http://localhost:3222/generateAccessToken"
 // );
- 
-window.truliooClient = new TruliooClient();
+
 window.TruliooClient = TruliooClient;
+//window.TruliooClient = TruliooClient;
 
-window.addEventListener("DOMContentLoaded", function () {
-    console.log('CURRENT VALUES:', truliooClient)
-    const embedIDBackendURL = "http://localhost:8855/embedid/";
-    const element = document.createElement('iframe');
-    element.setAttribute('id', 'embedid-module');
-    element.setAttribute('src', `${embedIDBackendURL}${truliooClient.publicKey}`);
-    document.getElementById('trulioo-embedid').appendChild(element);
 
-    window.addEventListener('message', async (e) => {
-        const originURL = 'http://localhost:8855'; //embedID BE URL
-        if (e.origin === originURL) {
-            const response = await fetch(`${truliooClient.accessTokenGeneratorURL}`);
-            const accessToken = JSON.parse(response.data.AccessToken);
-            //console.log('Sending Access Access Token fron client to EmbedID-BE:', accessToken);
-            //e.source.postMessage(`Ihackedyou;D`, '*');
-            //TODO change
-            e.source.postMessage(`${accessToken}`, '*');
-        }
-    });
+// window.addEventListener("DOMContentLoaded", function () {
+//     console.log('CURRENT VALUES:', truliooClient)
+//     const embedIDBackendURL = "http://localhost:8855/embedid/";
+//     const element = document.createElement('iframe');
+//     element.setAttribute('id', 'embedid-module');
+//     element.setAttribute('src', `${embedIDBackendURL}${truliooClient.publicKey}`);
+//     document.getElementById('trulioo-embedid').appendChild(element);
 
-    const embedIDModule = document.getElementById("embedid-module");
-    embedIDModule.style.width = "100%";
-    embedIDModule.style.height = "100%";
-    embedIDModule.border = "none";
-}, false);
+//     window.addEventListener('message', async (e) => {
+//         const originURL = 'http://localhost:8855'; //embedID BE URL
+//         if (e.origin === originURL) {
+//             const response = await fetch(`${truliooClient.accessTokenGeneratorURL}`);
+//             const accessToken = JSON.parse(response.data.AccessToken);
+//             //console.log('Sending Access Access Token fron client to EmbedID-BE:', accessToken);
+//             //e.source.postMessage(`Ihackedyou;D`, '*');
+//             //TODO change
+//             e.source.postMessage(`${accessToken}`, '*');
+//         }
+//     });
+
+//     const embedIDModule = document.getElementById("embedid-module");
+//     embedIDModule.style.width = "100%";
+//     embedIDModule.style.height = "100%";
+//     embedIDModule.border = "none";
+// }, false);
