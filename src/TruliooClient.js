1ace13f23e3e12826d6c30c9e4a31fa7cba94356
mkotsollaris <mkotsollari@gmail.com>
2020-01-19 17:12:39 -0800

    updated authentication workflow

--- src/TruliooClient.js
+++ src/TruliooClient.js
@@ -4,14 +4,30 @@ export default class TruliooClient {
         for (var fld in obj) {
             this[fld] = obj[fld];
         }
+        this.init();
+    }
+
+    async init() {
+        await this.injectAccessToken();
         this.loadEmbedID();
+    }
+
+    async injectAccessToken() {
+        const originURL = 'http://localhost:3010/trulioo-api/embedids/tokens'; //embedid-be-sdk
+        const response = await fetch(originURL, {
+            method: 'POST'
+        });
+        const deconstructedResult = await response.json();
+        const accessToken = deconstructedResult.accessToken;
+        this['accessToken'] = accessToken;
     }
 
     loadEmbedID() {
         const embedIDBackendURL = "http://localhost:8855/embedid/";
+        const url = `${embedIDBackendURL}${this.publicKey}/at/${this.accessToken}`;
         const element = document.createElement('iframe');
         element.setAttribute('id', 'embedid-module');
-        element.setAttribute('src', `${embedIDBackendURL}${this.publicKey}`);
+        element.setAttribute('src', url);
         const truliooEmbedIDContainer = document.getElementById('trulioo-embedid')
         truliooEmbedIDContainer.appendChild(element);
         truliooEmbedIDContainer.style.paddingTop = "100%";
