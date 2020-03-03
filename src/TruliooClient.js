84200e88da456b7abc0c8fafafb610b4fed3d163
brandonv-dev <bverigin95@gmail.com>
2020-03-02 19:22:40 -0800

    made so non 200 responses dont get used for access token

--- src/TruliooClient.js
+++ src/TruliooClient.js
@@ -67,6 +67,9 @@ export default class TruliooClient {
         method: 'POST'
       });
       const deconstructedResult = await response.json();
+      if (response.status !== 200) {
+        throw new Error(deconstructedResult.error || deconstructedResult);
+      }
       const accessToken = deconstructedResult.accessToken;
       this.accessToken = accessToken;
     } catch (error) {
